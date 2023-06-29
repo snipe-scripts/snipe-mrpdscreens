80970f81cf7fd48552e67020de452fd51d8c5a20
Snipe <10562121+pushkart2@users.noreply.github.com>
2023-06-29 16:07:44 -0500

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,6 @@
+-----------------For support, scripts, and more----------------
+--------------- https://discord.gg/VGYkkAYVv2  -------------
+---------------------------------------------------------------
 QBCore = nil
 local ScreenInfo = {}
 
